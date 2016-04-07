5daddc2dbece78a4965a8eae418ffa6b67aa4e85
David Fioretti <fioretti.david@gmail.com>
2016-04-07 10:51:28 -0400

    fix layout

--- scripts/components/common/charts/ScoreView.jsx
+++ scripts/components/common/charts/ScoreView.jsx
@@ -3,6 +3,8 @@ var uuid = require('node-uuid');
 var ComponentOverlay = require('./ComponentOverlay.jsx');
 var Navigation = require('react-router').Navigation;
 var Fluxxor = require('fluxxor');
+var DataFormatter = require('../../../utils/DataFormatter.js');
+///var DataFomatter = require('../../../utils/DataFormatter.js');
 var FluxMixin = Fluxxor.FluxMixin(React);
 
 var ScoreView = React.createClass({
@@ -12,19 +14,21 @@ var ScoreView = React.createClass({
 		return { scoreId: uuid.v4(), sliderId: uuid.v4() };
 	},
 	componentDidMount: function() {
+		console.log("DID MT", this.props);
 		var val = 0.0;
 		if (typeof(this.props.component.state) !== 'undefined' &&
 		this.props.component.state.data.length > 0) {
-			val = this.props.component.state.data[0].value / 100;
+			val = this.props.component.state.data[0].value;// / 100;
 		}
 		var pos = (292 * val / 1) -8;
+		console.log("pos", pos);
 		$('#' + this.state.sliderId).animate({left: pos}, 1000);
 	},
 	componentWillReceiveProps: function(newProps) {
 		var val = 0.0;
 		if (typeof(newProps.component.state) !== 'undefined'
 			&& newProps.component.state.data.length > 0) {
-				val = newProps.component.state.data[0].value / 100;
+				val = newProps.component.state.data[0].value;// / 100;
 		}
 		var pos = (292 * val / 1) - 8;
 		$('#' + this.state.sliderId).animate({left:pos}, 1000);
@@ -46,8 +50,7 @@ var ScoreView = React.createClass({
 	  this.transitionTo('/apt/editor_score/' + score_id);
 	},
 	render: function() {
-		var left = 0.8 * 300 - 30;
-		var tooltipStyle = { left: left, top: -66, backgroundColor: "#97c93c" };
+
 		var arrowStyle = { borderTop: "20px solid " + "#97c93c" };
 		var metric = "";
 		var score = "#";
@@ -56,7 +59,9 @@ var ScoreView = React.createClass({
 			metric = this.props.component.state.data[0].metric.split("_").join(" ");
 			score = this.props.component.state.data[0].value;
 		}
-		score = Math.floor(Math.random() * 30) + 70;
+		var left = score * 300 - 30;
+		var tooltipStyle = { left: left, top: -66, backgroundColor: "#97c93c" };
+		//score = Math.floor(Math.random() * 30) + 70;
 		return (
 			<div id="risk_assessment" className="dashboard-module">
 				<div className="top">
@@ -72,12 +77,12 @@ var ScoreView = React.createClass({
 					<div className="top-title">{this.props.component.name}</div>
 				</div>
 				<div className="main">
-					<div className="risk">{score}</div>
+					<div className="risk">{Math.round(DataFormatter(score) * 10) / 10}</div>
 					<div className="subheader">{metric}</div>
 					<div className="slider-bar" onMouseOver={this.showTooltip} onMouseLeave={this.hideTooltip}>
 						<div id={this.state.sliderId} className="slider-button"></div>
 						<div id={this.state.scoreId} className="custom-tooltip" style={tooltipStyle}>
-							<span className="risk-label">{score}/100</span>
+							<span className="risk-label">{Math.round(DataFormatter(score))}/100</span>
 							<div className="custom-tooltip-arrow" style={arrowStyle}></div>
 						</div>
 					</div>
