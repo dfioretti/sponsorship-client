e2d4e412818d3861697d69e3b3d00797b51a740d
David Fioretti <fioretti.david@gmail.com>
2016-07-19 17:22:24 -0400

    updating

--- scripts/constants/environment.js
+++ scripts/constants/environment.js
@@ -1,4 +1,32 @@
+getApiRoot = function() {
+    var url = location.href.split(':');
+    // dev
+    if (url.length == 3) {
+        console.log("wtf");
+        return "" + url[0] + ":" + url[1] + ":4000/";
+    } else {
+        console.log("wtf2");
+        url = location.href.split('.');
+        return "" + url[0] + "teneosponsorship.herokuapp.com/";
+    }
+}
+
 module.exports = {
+    /*
 	//API_ROOT: "http://teneo-sponsorship-api.herokuapp.com/"
-	API_ROOT: (location.href.indexOf('localhost') > -1) ? "http://localhost:4000/" : "https://teneo-sponsorship-api.herokuapp.com/"
+    API_ROOT = function() {
+        var url = location.href.split(':');
+        // dev
+        if (url.length == 3) {
+            return "" + url[0] + ":" + url[1] + ":4000/";
+        } else {
+            url = location.href.split('.');
+            return "" + url[0] + "teneo-sponsorship-api.herokuapp.com/";
+        }
+    }
+    */
+    API_ROOT: getApiRoot()
+    //API_ROOT: getApiUrl()
+    //API_ROOT: (location.href.indexOf('localhost') > -1) ? "http://localhost:4000/" : "https://teneo-sponsorship-api.herokuapp.com/"
+
 };
